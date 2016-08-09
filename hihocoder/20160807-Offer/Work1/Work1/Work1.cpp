a946c49440d01e42dfacf32924ace5a30a594fe7
yanjiankang <healthyjk@gmail.com>
2016-08-09 16:20:59 +0800

    更新了满减优惠题目答案

--- hihocoder/20160807-Offer/Work1/Work1/Work1.cpp
+++ hihocoder/20160807-Offer/Work1/Work1/Work1.cpp
@@ -9,31 +9,55 @@
 #include <algorithm>
 #include <fstream>
 
+#define INT_MIN     (-2147483647 - 1) /* minimum (signed) int value */
+#define INT_MAX       2147483647      /* maximum (signed) int value */
+
 using namespace std;
 
-const int maxn = 10001;
-int n, m;
-bool f[maxn];
-
+/* 经验教训： 第一题永远是暴力解就能过的。。。SX。。。
+   想不出好的解就用暴力解； */
 int main()
 {
-	ifstream cin("input.txt");
-	cin >> n >> m;
-	f[0] = true;
-	for (int i = 0; i < n; i++)
-	{
-		int x;
-		cin >> x;
-		for (int j = maxn - 1; j >= x; j--)
-			f[j] |= f[j - x];
-	}
+  ifstream cin("input.txt");
 
-	for (int i = m; i < maxn; i++)
-		if (f[i])
-		{
-			printf("%d\n", i);
-			return 0;
-		}
-	puts("-1");
+  int n, x;
+  cin >> n >> x;
+  vector<int> nums(n);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> nums[i];
+  }
+
+  int result = INT_MAX;
+  int current = 0;
+
+  for (int i = 0; i < n; ++i) {
+    current += nums[i];
+  }
+  if (current < x) {
+    cout << "-1" << endl;
+    return;
+  }
+
+  int MaxKind = 1 << n;
+  for (int i = 0; i < MaxKind; ++i) {
+    current = 0;
+    //将i看成20位的二进制数字，i的每一位代表第j个数是否选中
+    for (int j = 0; j < n; ++j) {
+      if (((1 << j) & i) != 0) {
+        current += nums[j];
+        if (current >= x) {
+          break;
+        }
+      }
+    }
+
+    if (current >= x && current < result) {
+      result = current;
+    }
+  }
+
+  cout << result << endl;
+  return 0;
 }
 
