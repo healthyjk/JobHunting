393d2471d23fc276f4803298e8fdf5d4fe88b780
YanJiankang <healthyjk@gmail.com>
2016-09-27 22:29:43 +0800

    normal commit

---
+++ CPPUnitTest/DemosTest/TempTest.cpp
@@ -0,0 +1,61 @@
+#include "UnitTest++/UnitTest++.h"
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "EasyLogging/easylogging++.h"
+
+using namespace std;
+
+/**
+  * 测试用例：代码测试模版用于拷贝
+  * 作者：yanjiankang
+  * 时间：2016年8月14日
+  **/
+
+SUITE(TempTest)
+{
+    class Solution {
+    public:
+        int combinationSum4(vector<int>& nums, int target) {
+            int n = nums.size();
+            if (n <= 0) return 0;
+            sort(nums.begin(), nums.end());
+            int count = 0;
+            dfs(nums, n, target, 0, 0, count);
+            return count;
+        }
+
+        void dfs(vector<int>& nums, int n, int target, int cur, int sum, int& count) {
+            if (cur >= n || sum >= target) {
+                if (sum == target) {
+                    count++;
+                }
+                return;
+            }
+            int k = nums[cur];
+            int limit = target / k;
+            for (int i = 0; i <= limit; ++i) {
+                dfs(nums, n, target, cur+1, sum + i * k, count);
+            }
+        }
+    };
+
+    TEST_FIXTURE(Solution, Normal1)
+    {
+        vector<int> nums = {1,2,3};
+        int target = 4;
+        int result = combinationSum4(nums, target);
+        LOG(INFO) << "combination result:" << result << endl;
+        return;
+    }
+
+    TEST_FIXTURE(Solution, Normal2)
+    {
+        return;
+    }
+
+    TEST_FIXTURE(Solution, Normal3)
+    {
+        return;
+    }
+}
---
+++ CPPUnitTest/PatternTest/BridgeTest.cpp
@@ -0,0 +1,151 @@
+#include "UnitTest++/UnitTest++.h"
+#include <vector>
+#include <iostream>
+#include <string>
+#include "EasyLogging/easylogging++.h"
+
+using namespace std;
+
+/**
+  * 测试用例：桥接模式
+  * 设计模式介绍：桥接模式(Bridge Pattern)：将抽象部分与它的实现部分分离，使它们都可以独立地变化。
+  * 它是一种对象结构型模式，又称为柄体(Handle and Body)模式或接口(Interface)模式。
+  * 作者：yanjiankang
+  * 时间：2016年9月16日
+  **/
+
+class Implementation {
+public:
+    virtual void operationImpl() {
+        LOG(INFO) << "BaseI mplementation!";
+    }
+};
+
+class ImplementationA : public Implementation {
+public:
+    void operationImpl() {
+        LOG(INFO) << "A mplementation!";
+    }
+};
+
+class ImplementationB : public Implementation {
+public:
+    void operationImpl() {
+        LOG(INFO) << "A mplementation!";
+    }
+};
+
+class Abstration {
+public:
+    Abstration(Implementation* implementation) {
+        implementation_ = implementation;
+    }
+
+    virtual ~Abstration() {
+        if (NULL != implementation_) {
+            delete implementation_;
+        }
+    }
+
+    void operation(){
+        implementation_->operationImpl();
+    }
+
+    Implementation* implementation_;
+};
+
+class RefinedAbstration : public Abstration {
+public:
+    RefinedAbstration(Implementation* impl) : Abstration(impl)  {}
+    void operation(){
+        LOG(INFO) << "do something else, and then";
+        implementation_->operationImpl();
+    }
+};
+
+
+/*以下为桥接模式应用实例*/
+class OS {
+public:
+    OS() {}
+
+    virtual void install() {
+        LOG(INFO) << "Install OS";
+    }
+};
+
+class PC {
+public:
+    PC(OS* os) {
+        os_ = os;
+    }
+
+    ~PC() {
+       if (NULL != os_) {
+           delete os_;
+       }
+    }
+
+    void installOS() {
+        LOG(INFO) << getPCType();
+        os_->install();
+    }
+
+    virtual string getPCType() {
+        string type("Normal PC");
+        return type;
+    }
+
+public:
+    OS* os_;
+};
+
+class WindowsOS : public OS {
+public:
+    virtual void install() {
+        LOG(INFO) << "Install Windows OS";
+    }
+};
+
+class LinuxOS : public OS {
+public:
+    virtual void install() {
+        LOG(INFO) << "Install Linux OS";
+    }
+};
+
+class HP_PC : public PC {
+public:
+    HP_PC(OS* os) : PC(os) {}
+    string getPCType() {
+        string type("HP PC");
+        return type;
+    }
+};
+
+SUITE(BridgeTest)
+{
+    TEST(Normal1)
+    {
+        Implementation* impl = new ImplementationA();
+        RefinedAbstration* refineAbs_a = new RefinedAbstration(impl);
+
+        Implementation* imp2 = new ImplementationB();
+        RefinedAbstration* refineAbs_b = new RefinedAbstration(imp2);
+
+        refineAbs_a->operation();
+        refineAbs_b->operation();
+        delete refineAbs_a;
+        delete refineAbs_b;
+        delete impl;
+        delete imp2;
+    }
+
+    TEST(Normal2) {
+        OS* winos = new WindowsOS();
+        HP_PC* hp = new HP_PC(winos);
+        hp->installOS();
+        delete winos;
+        delete hp;
+    }
+}
