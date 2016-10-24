e9a3f6cb16fd56cc6db2fc2c5198b9d33ab6bdd3
yanjiankang <healthyjk@gmail.com>
2016-10-24 08:55:28 +0800

    normal commit

--- CPPUnitTest/AlgorithmTest/LongestSubStrSeqTest.cpp
+++ CPPUnitTest/AlgorithmTest/LongestSubStrSeqTest.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 /**
-  * 测试用例：最长公共子序列用例测试
+  * 测试用例：最长公共子序列用例测试 && 最长公共子串
   * 作者：yanjiankang
   * 时间：2016年9月6日
   **/
@@ -52,6 +52,77 @@ SUITE(LongestSubStrSeqTest)
             LOG(INFO) << result;
             return dp[n][m];
         }
+
+        int LongestSubString(string& str1, string& str2) {
+            int n = str1.size();
+            int m = str2.size();
+
+            vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+            int max_len = 0;
+
+            for (int i = 1; i <= n; ++i) {
+                for (int j = 1; j <= m; ++j) {
+                    //cout << str1[i-1] << str2[j-1] << endl;
+                    if (str1[i-1] == str2[j-1]) {
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                        if (max_len < dp[i][j]) {
+                            max_len = dp[i][j];
+                        }
+                    } else {
+                        dp[i][j] = 0;
+                    }
+                }
+            }
+            return max_len;
+        }
+
+        int LongestSubString2(string& str1, string& str2) {
+            int n = str1.size();
+            int m = str2.size();
+
+            int max_len = 0;
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    int s = i;
+                    int t = j;
+                    int length = 0;
+
+                    while (s < n && t < m && str1[s-1] == str2[t-1]) {
+                        s++;
+                        t++;
+                        length++;
+                    }
+
+                    if (length > max_len) {
+                        max_len = length;
+                    }
+                }
+            }
+            return max_len;
+        }
+
+        int LongestSubString3(string& str1, string& str2) {
+            int n = str1.size();
+            int m = str2.size();
+
+            vector<int> dp(m+1, 0);
+            int max_len = 0;
+
+            for (int i = 1; i <= n; ++i) {
+                for (int j = m; j >= 1; --j) {
+                    //cout << str1[i-1] << str2[j-1] << endl;
+                    if (str1[i-1] == str2[j-1]) {
+                        dp[j] = dp[j-1] + 1;
+                        if (max_len < dp[j]) {
+                            max_len = dp[j];
+                        }
+                    } else {
+                        dp[j] = 0;
+                    }
+                }
+            }
+            return max_len;
+        }
     };
 
     TEST_FIXTURE(Solution, Normal1)
@@ -65,6 +136,15 @@ SUITE(LongestSubStrSeqTest)
 
     TEST_FIXTURE(Solution, Normal2)
     {
+        string str1 = "abcabced";
+        string str2 = "ubcadefer";
+        int len = LongestSubString3(str1, str2) ;
+        cout << "Max Longest Substr len:" << len << endl;
+
+        str1 = "YXXXXXY";
+        str2 = "YXYXXYYYYXXYYYYXYYXXYYXXYXYYYYYYXYXYYXYXYYYXXXXXX";
+        len = LongestSubString3(str1, str2) ;
+        cout << "Max Longest Substr len:" << len << endl;
         return;
     }
 
